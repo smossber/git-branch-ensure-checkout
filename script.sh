620e26d964cd36f2cabb8675daaf76f7053a2adb
Sebastian Mossberg <smossber@redhat.com>
2020-01-29 10:49:57 +0100

    More printout and set a variable of where to checkout the branches

--- script.sh
+++ script.sh
@@ -3,20 +3,22 @@
 
 set -e
 
+puppet_environment_directory="./puppet-environments/"
+
 # Check if the ref directory already exists
-if [[ -d ./branches/ref/ ]]; then
+if [[ -d ${puppet_environment_directory}ref/ ]]; then
 	echo "Seems like the ref directory is already checked out"
-	pushd ./branches/ref/
+	pushd ${puppet_environment_directory}ref/
 
 	echo "git fetch to get the latest from origin"
 	git fetch --all
 	popd 
 else
 	echo "Seems like we haven't checked out the ref yet"
-	mkdir -p ./branches/
+	mkdir -p ${puppet_environment_directory}
 	
 	echo "Check out the ref directory"
-	git clone git@github.com:smossber/git-branch-ensure-checkout.git ./branches/ref/
+	git clone git@github.com:smossber/git-branch-ensure-checkout.git ${puppet_environment_directory}ref/
 fi
 	
 
@@ -24,12 +26,15 @@ fi
 #git clone --bare user@git-server:/your-repo.git && cd your-repo.git
 
 echo "Moving in to ref branch" 
-pushd ./branches/ref/
+pushd ${puppet_environment_directory}ref/
 
 # For each branch in the remote origin
 # Except HEAD,master and any output that contains "*" (current branch)
 # copy the directory (to avoid another git clone.. )
 # and checkout that branch
+echo "What branches are available:"
+git branch -a | sed -e 's|remotes/origin/||g' | sed -e 's/ //g' | egrep -v 'HEAD|master|\*'
+
 git branch -a | sed -e 's|remotes/origin/||g' | sed -e 's/ //g' | egrep -v 'HEAD|master|\*'| while read branchName; do 
   echo ""
   #branchName=${branchRef#refs/heads/}
@@ -41,6 +46,6 @@ git branch -a | sed -e 's|remotes/origin/||g' | sed -e 's/ //g' | egrep -v 'HEAD
   popd
 done
 
-  #git clone git@github.com:smossber/git-branch-ensure-checkout.git ../../branches/${branchName}
+  #git clone git@github.com:smossber/git-branch-ensure-checkout.git ../.${puppet_environment_directory}${branchName}
   #git archive --format=tar --prefix="$branchName/" "$branchRef" | tar -C/var/www/html -x
 popd
