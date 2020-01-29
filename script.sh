bbd0c13724708022d93c50909e64721c054c7558
Sebastian Mossberg <smossber@redhat.com>
2020-01-29 10:35:35 +0100

    swap for git branch -a instead as I dont seem to get the remote branches with got for-each-ref

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# 
 # Forked from https://stackoverflow.com/questions/46102041/git-get-worktree-for-every-branch-in-seperate-folders-bash
+
 set -e
 
 # Check if the ref directory already exists
@@ -9,7 +9,7 @@ if [[ -d ./branches/ref/ ]]; then
 	pushd ./branches/ref/
 
 	echo "git fetch to get the latest from origin"
-	git fetch
+	git fetch --all
 	popd 
 else
 	echo "Seems like we haven't checked out the ref yet"
@@ -26,16 +26,21 @@ fi
 echo "Moving in to ref branch" 
 pushd ./branches/ref/
 
-git for-each-ref --format='%(refname)' refs/heads/ | while read branchRef; do
+# For each branch in the remote origin
+# Except HEAD,master and any output that contains "*" (current branch)
+# copy the directory (to avoid another git clone.. )
+# and checkout that branch
+git branch -a | sed -e 's|remotes/origin/||g' | sed -e 's/ //g' | egrep -v 'HEAD|master|\*'| while read branchName; do 
   echo ""
-  branchName=${branchRef#refs/heads/}
+  #branchName=${branchRef#refs/heads/}
 
   echo "Creating Branch: ${branchName}"
   cp -rf ./ ../${branchName}
   pushd ../${branchName}
   git checkout -B ${branchName} origin/${branchName} -f
   popd
+done
 
   #git clone git@github.com:smossber/git-branch-ensure-checkout.git ../../branches/${branchName}
   #git archive --format=tar --prefix="$branchName/" "$branchRef" | tar -C/var/www/html -x
-done
+popd
